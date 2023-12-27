e6cb95451267d69c06e0d98e3795ab18a632975c
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-27 09:26:28 +0500

    some changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
                 <Image src="/youtube.png" alt="youtube" width={24} height={24} />
             </div>
-            <div className={styles.logo}>ravablog</div>
+            <div className={styles.logo}><Link href="/">ravablog</Link></div>
             <div className={styles.links}>
                 <ThemeToggle />
                 <Link href="/" className={styles.link}>Homepage</Link>
